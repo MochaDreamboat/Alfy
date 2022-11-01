7c26f827364e2b7362fa143f92ba1e8634abc899
mochadreamboat <jersotmac@gmail.com>
2022-10-31 23:36:22 -0500

    Added more methods

--- FCC Python/08_lists.py
+++ FCC Python/08_lists.py
@@ -11,5 +11,33 @@ print(friends[1:3]) # Returns ['Sowji', 'Annie']
 
 numbers = [1, 2, 3, 4, 5]
 
-numbers.extend(friends) # Extends numbers param w/ friends list (returns None)
+# .extend(iterable)
+numbers.extend(friends) # Extends list with iterable (list, tuple, string, etc.)
 print(numbers)
+
+# .append(element)
+numbers.append('add me!') # appends element to end of List
+
+#.insert(index, element)
+numbers.insert(3, "Tracey") # Adds element at given index position
+
+#.remove(element)
+numbers.remove("Tracey") # Removes first instance of element in list
+
+# .pop()
+numbers.pop() # Removes last element from list
+
+#.index("element")
+numbers.index("Sowji") # Returns index of element's first appearance
+
+#.clear()
+numbers.clear() # Removes ALL items in List. Careful!
+
+#.count(element)
+numbers.count("Sowji") # Counts number of instances of element
+
+# .sort()
+numbers.sort() # Sorts List in ascending order
+
+# .reverse()
+numbers.sort() # Reverse list (back to front)
