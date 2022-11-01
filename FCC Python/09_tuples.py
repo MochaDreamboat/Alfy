9c95672409ea1b87bd5fc01eba43940811271431
mochadreamboat <jersotmac@gmail.com>
2022-10-31 23:36:41 -0500

    Added tuples lesson.

---
+++ FCC Python/09_tuples.py
@@ -0,0 +1,12 @@
+# Tuple
+# Structure where you can store multiple pieces of info
+# Like list, but immutable
+# Used for constant data
+# (i.e. coordinates[1] = 2 : generates error)
+
+
+# Creating a tuple
+coordinates = (4, 5) #  Used open, closed parentheses
+
+# Accessing tuple value
+print(coordinates[0])
