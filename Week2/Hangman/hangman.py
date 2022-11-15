c1758d44dac4229835035f12cc2fbd7f5b2aec1f
mochadreamboat <jersotmac@gmail.com>
2022-11-14 20:07:33 -0600

    Constructed game flow via main function.

--- Week2/Hangman/hangman.py
+++ Week2/Hangman/hangman.py
@@ -1,4 +1,3 @@
-from wsgiref import validate
 from hangman_helper import *
 
 def update_word_pattern(word, pattern, letter):
@@ -26,18 +25,21 @@ def validate_word(word):
         if validate_entry(char) == False:
             return False
         return True
-    
+
+def filter_words_list(words, pattern, wrong_guess_list):
+    None
+
 def run_single_game(list_words, score=POINTS_INITIAL):
     word_to_guess = get_random_word(list_words)
     pattern = "_" * len(word_to_guess)
     wrong_guess_lst = []
+    game_over = False
 
     print(word_to_guess)
 
-    game_over = False
     message = "Guess a letter/word...or ask for a hint!"
 
-    while game_over == False or score != 0:
+    while game_over == False:
         display_state(pattern, wrong_guess_lst, score, message)
         choice = get_input()
         entry = choice[1]
@@ -46,7 +48,7 @@ def run_single_game(list_words, score=POINTS_INITIAL):
             if validate_entry(entry) == False or len(entry) > 1:
                 message = "Invalid input! Try again"
                 continue
-            elif entry in pattern or entry in wrong_guess_lst:
+            if entry in pattern or entry in wrong_guess_lst:
                 message = "Already guessed that! Try again"
                 continue
             
@@ -59,10 +61,10 @@ def run_single_game(list_words, score=POINTS_INITIAL):
             appearances = word_to_guess.count(entry)
             pattern = update_word_pattern(word_to_guess, pattern, entry)
             score += (appearances * (appearances+1) // 2)
-            continue
+            message = 'Keep guessing!'
 
         if choice[0] == WORD:
-            if validate_word == False:
+            if validate_word(entry) == False:
                 message = 'Please enter a valid word.'
                 continue
 
@@ -70,9 +72,41 @@ def run_single_game(list_words, score=POINTS_INITIAL):
             unsolved = pattern.count("_")
             if entry == word_to_guess:
                 pattern = word_to_guess
-                game_over = True
                 score += (unsolved*(unsolved-1) // 2)
+            else:
+                wrong_guess_lst.append(entry)
+                message = 'That\'s not the word. Try again!'
+
+        if pattern == word_to_guess:
+            message = 'Congrats! You got the word!'
+            game_over = True
+        elif score == 0:
+            message = f'Sorry, bud! The answer was {word_to_guess}'
+
+    display_state(pattern, wrong_guess_lst, score, message)
+
+    return score
 
 
-words = load_words()
-run_single_game(words)
+def main():
+    games_played = 0
+    words = load_words()
+    active_game = True
+    current_points = POINTS_INITIAL
+
+    while active_game == True:
+        current_points = run_single_game(words, current_points)
+        
+        if current_points > 0:
+            games_played += 1
+            active_game = play_again(f"""
+            You have scored {current_points} so far across {games_played} games.
+            Play again?""")
+
+        elif current_points == 0:
+            current_points = POINTS_INITIAL
+            active_game = play_again(f"You have lasted for {games_played} games. Wanna give it another go?")
+            games_played = 0
+
+if __name__ == "__main__":
+    main()
