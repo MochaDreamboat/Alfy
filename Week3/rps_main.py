6dad415e0a2f103cf41c426433e1cd8ff75ef6be
mochadreamboat <jersotmac@gmail.com>
2022-11-17 19:04:21 -0600

    Completed game flow.

--- Week3/rps_main.py
+++ Week3/rps_main.py
@@ -2,24 +2,38 @@ from rps_helper import *
 
 def main():
 
-    option = input('Welcome to Rock, Paper, Scissors!\nOne or two players?: ')
+    option = check_input('Welcome to Rock, Paper, Scissors!\nOne or two players?: ')
     game_over = False
-    player1 = Player(input('Player One, select your name: '))
+    player1 = Player(check_input('Player One, select your name: '))
 
     if option == '1':
         player2 = Player('Computer')
     else:
-        player2 = Player(input('Player Two, select your name: '))
+        player2 = Player(check_input('Player Two, select your name: '))
     
 
     while game_over == False:
         player1.choice = make_selection(player1)
         player2.choice = make_selection(player2) if option == '2' else randomize_choice()
 
+        print(f'{player1.name} chose {player1.choice}, {player2.name} chose {player2.choice}')
         determine_round(player1, player2)
         game_over = check_win(player1, player2)
         display_points(player1, player2)
-    
-    print('Want to play another round?')
 
-main()
+    # Asks player if they want to play again.
+    while True:
+        response = check_input('Want to go another round?')
+        if  response == 'y':
+            print('Let\'s roll!\n\n')
+            return main()
+        elif response == 'n':
+            print('Thanks for playing!')
+            exit()
+        else:
+            print('Make up your mind!')
+            continue
+
+
+if __name__ == "__main__":
+    main()
