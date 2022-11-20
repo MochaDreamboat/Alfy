64c9931b27608079e1e41f09ec7b2f5629865ac4
mochadreamboat <jersotmac@gmail.com>
2022-11-19 21:13:29 -0600

    Renamed variable to image_to_divide

--- Week3/Image_Editor/image_editor.py
+++ Week3/Image_Editor/image_editor.py
@@ -2,9 +2,14 @@ from image_editor_helper import *
 import copy
 
 def divide_channels(image):
-    image_to_edit = copy.deepcopy(image)
-    rows = len(image_to_edit)
-    columns = len(image_to_edit[0])
-    channels = len(image_to_edit[0][0])
+    image_to_divide = copy.deepcopy(image)
+    rows = len(image_to_divide)
+    columns = len(image_to_divide[0])
+    channels = len(image_to_divide[0][0])
 
-    return [ [[image_to_edit[j][k][i] for k in range(columns)] for j in range(rows)] for i in range(channels) ]
+    return [ [[image_to_divide[j][k][i] for k in range(columns)] for j in range(rows)] for i in range(channels) ]
+
+def merge_channels(image):
+  image_to_merge = copy.deepcopy(image)
+
+  
