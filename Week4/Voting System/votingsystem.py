215e93f870be2aff3777432f008456b3594e730f
mochadreamboat <jersotmac@gmail.com>
2022-11-27 22:39:19 -0600

    Finished plurality function.

--- Week4/Voting System/votingsystem.py
+++ Week4/Voting System/votingsystem.py
@@ -11,16 +11,36 @@ def file_loader(path, file_type):
             voting_data = json.load(votes)
     
     elif file_type == 'csv':
-        voting_data = {}
         with open(path, 'r') as votes:
             voting_reader = csv.reader(votes)
-            voting_data['options'] = next(voting_reader)
+
+            voting_data = {
+                'options': next(voting_reader)
+            }
 
             for vote in voting_reader:
                 voting_data[f'vote_{len(voting_data)}'] = vote
-            
+
     return voting_data
 
 
+'''
+Receives dictionary of options and votes. Tallies votes and returns first, second and third
+place winner. Vote determined by first preference of voter.
+'''
 def plurality(votes):
-    pass
+    preferences = votes['options']
+    tally = {option:0 for i, option in enumerate(votes['options'])}
+    
+    del votes['options']
+
+    for voter in votes.keys():
+        tally[preferences[int(votes[voter][0])-1]] += 1
+
+    preferences = sorted(preferences, key=lambda preference: tally[preference], reverse=True)
+    
+    return {
+        f'{i+1}': preferences[i] for i in range(len(preferences))
+    }
+
+print(plurality(file_loader('./votes/abc_True.csv', 'csv')))
