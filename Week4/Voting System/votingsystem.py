5ab0b5f47419eb50363ccbdd939b3d3cfd63f93d
mochadreamboat <jersotmac@gmail.com>
2022-11-27 20:05:00 -0600

    Initial commit / created file loader function.

---
+++ Week4/Voting System/votingsystem.py
@@ -0,0 +1,30 @@
+import json
+import csv
+
+'''
+Returns Python dictionary containing options key w/ array of options
+and each vote with an array of preferences. Accepts either csv or json.
+'''
+def file_loader(path, file_type):
+    if file_type == 'json':
+        with open(path) as votes:
+            voting_data = json.load(votes)
+    
+    elif file_type == 'csv':
+        voting_data = {}
+        with open(path, 'r') as votes:
+            voting_reader = csv.reader(votes)
+            voting_data['options'] = next(voting_reader)
+
+            for vote in voting_reader:
+                voting_data[f'vote_{len(voting_data)}'] = vote
+
+            print(voting_data['vote_1'])
+
+            return voting_data
+            
+    return voting_data
+
+
+def plurality(votes):
+    pass
